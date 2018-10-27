17011ad466937115cec4f80b226525a4656da43b
github username <github email address>
2018-10-27 14:39:56 -0400

    add AMS gem

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,20 @@
 class ExercisesController < ApplicationController
+
+
+  def index
+    @exercises = Exercise.all
+    render 
+  end
+
+  def new
+    @exercise = Exercise.create(exercise_params)
+  end
+
+
+
+  private
+
+    def exercise_params
+      params.require(:name).permit(:category, :equipment, :description, :muscle)
+    end
 end
